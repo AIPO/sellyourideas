ef1c3146c5d0144052a9de9e036e11b8c9d4b8f0
povilas <p.vaitonis@gmail.com>
2018-05-16 11:48:52 +0300

    Added fake data to database

--- database/seeds/IdeasTableSeeder.php
+++ database/seeds/IdeasTableSeeder.php
@@ -2,15 +2,15 @@
 
 use Illuminate\Database\Seeder;
 
-class DatabaseSeeder extends Seeder
+class IdeasTableSeeder extends Seeder
 {
     /**
-     * Seed the application's database.
+     * Run the database seeds.
      *
      * @return void
      */
     public function run()
     {
-        $this->call(IdeasTableSeeder::class);
+        factory(App\Idea::class, 30)->create();
     }
 }
